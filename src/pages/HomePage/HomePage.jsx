10ac093d3a4be173a81eeb167a209bd87b1402de
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-08-09 00:55:38 -0400

    Adding link to ComponentPage in HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import * as Components from "a11y-components";
+import styles from "./HomePage.module.scss";
 
-const HomePage = () => <h1>Home page works!</h1>;
+const HomePage = () => {
+  const componentNames = Object.keys(Components);
+  const [currentComponent, setCurrentComponent] = useState(
+    Components[componentNames[0]]
+  );
+
+  return (
+    <div className={styles.HomePage}>
+      <h2>What component do you want to see in the Playground?</h2>
+      {componentNames.map((name) => (
+        <Link to={`/component/${name}`}>{name}</Link>
+      ))}
+    </div>
+  );
+};
 
 export default HomePage;
