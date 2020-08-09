8170f5d33941217714fdc4b9af663a8c50fa3278
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-08-09 01:03:35 -0400

    Removing more unused code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import * as Components from "a11y-components";
 import styles from "./HomePage.module.scss";
