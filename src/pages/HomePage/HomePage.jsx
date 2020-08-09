603926baa2dfc4fac3a1d76bf0be59b639b828a3
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-08-09 01:01:17 -0400

    Removing some unused code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,20 +3,13 @@ import { Link } from "react-router-dom";
 import * as Components from "a11y-components";
 import styles from "./HomePage.module.scss";
 
-const HomePage = () => {
-  const componentNames = Object.keys(Components);
-  const [currentComponent, setCurrentComponent] = useState(
-    Components[componentNames[0]]
-  );
-
-  return (
-    <div className={styles.HomePage}>
-      <h2>What component do you want to see in the Playground?</h2>
-      {componentNames.map((name) => (
-        <Link to={`/component/${name}`}>{name}</Link>
-      ))}
-    </div>
-  );
-};
+const HomePage = () => (
+  <div className={styles.HomePage}>
+    <h2>What component do you want to see in the Playground?</h2>
+    {Object.keys(Components).map((name) => (
+      <Link to={`/component/${name}`}>{name}</Link>
+    ))}
+  </div>
+);
 
 export default HomePage;
