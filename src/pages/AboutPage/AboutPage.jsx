869dc876ff9f4e67a5ab582fa14fbc94a5149d53
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-20 19:16:50 -0400

    Adding content to the about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,5 +1,72 @@
-import React from "react";
+import React from 'react';
 
-const AboutPage = () => <h1>About page works!</h1>;
+const AboutPage = () => (
+  <>
+    <h2>About this Playground</h2>
+
+    <h3>First of all</h3>
+    <p>
+      It's important to know this site is a work in progress. Thanks for you
+      patience as it is developed along with the{' '}
+      <a
+        href="https://github.com/ashleemboyer/a11y-components"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <code>a11y-components</code>
+      </a>{' '}
+      component library. :)
+    </p>
+
+    <h3>Second of all</h3>
+    <p>
+      I'm glad you're here! That probably means you've heard about{' '}
+      <a
+        href="https://github.com/ashleemboyer/a11y-components"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <code>a11y-components</code>
+      </a>{' '}
+      and are interested in testing out the components from the library in an
+      interactive playground environment. You've come to the right place! Well,
+      it's actually{' '}
+      <a href="/" target="_blank" rel="noopener noreferrer">
+        just around the corner
+      </a>
+      , but you get it.
+    </p>
+
+    <h3>Okay, the point</h3>
+    <p>
+      A11Y Components Playground was created so that the components in the{' '}
+      <a
+        href="https://github.com/ashleemboyer/a11y-components"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <code>a11y-components</code>
+      </a>{' '}
+      component library could be tested live, by anyone, before installing the
+      library. The components are based off of the one's created by{' '}
+      <a
+        href="https://twitter.com/AshleeMBoyer"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Ashlee M Boyer
+      </a>{' '}
+      in her{' '}
+      <a
+        href="https://ashleemboyer.com/build-an-accessible-react-component-the-live-series"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Build an Accessible React Component
+      </a>{' '}
+      live-streaming series.
+    </p>
+  </>
+);
 
 export default AboutPage;
